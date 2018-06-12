8b4bcdeabff17efff0e54c70d715a5e79dc1217f
JoySarkar <joysarkar@JoySarkars-MacBook-Pro.local>
2018-06-12 08:54:11 -0400

    ec2 module

---
+++ ec2/main.tf
@@ -0,0 +1,20 @@
+# provider "aws" {
+#   region = "${var.aws_region}"
+# }
+
+resource "aws_instance" "ec2" {
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+
+  #availability_zone      = "${var.availability_zone}"
+  vpc_security_group_ids = ["${split(",", var.vpc_security_group_ids)}"]
+  key_name               = "${var.key_name}"
+  subnet_id              = "${var.subnet_id}"
+  iam_instance_profile   = "${var.iam_instance_profile}"
+
+  # user_data              = "${file("customScript/${var.userdatascript}")}"
+
+  tags {
+    Name = "${var.name}"
+  }
+}
---
+++ ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {}
+
+variable "ami" {}
+
+variable "instance_type" {}
+
+variable "vpc_security_group_ids" {}
+
+variable "key_name" {}
+
+variable "subnet_id" {}
+
+variable "iam_instance_profile" {}
+
+variable "name" {}
