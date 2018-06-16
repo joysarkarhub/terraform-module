067fff53b4e7e5fc40b51084dd4297189ab96a4c
JoySarkar <joysarkar@JoySarkars-MacBook-Pro.local>
2018-06-16 18:40:11 -0400

    debug

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,8 +1,9 @@
 resource "aws_vpc" "main" {
     cidr_block             = "${var.cidr_block}"
     instance_tenancy       = "${var.tenancy}"
+
+    tags {
+        Name = "${var.vpc_name}-VPC"
+    }
 }
 
-tags {
-    Name = "${var.vpc_name}-VPC"
-}
